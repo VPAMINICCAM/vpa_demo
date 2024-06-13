735ac29a0e00448af93d09bdb49dd7ffd895ff9f
Yikai Zeng <yikai.zeng@tu-dresden.de>
2024-06-13 15:53:09 +0200

    update remote start sh

--- env/remote_start_smart_inter.sh
+++ env/remote_start_smart_inter.sh
@@ -5,9 +5,9 @@ source /opt/ros/noetic/setup.bash
 # Source the catkin workspace
 source /home/vpaadmin/catkin_ws/devel/setup.bash
 
-# Export custom environment variables
-export ROBOT_NAME=$(hostname)  # Uses the hostname as the robot name, or replace $(hostname) with specific names if needed
-
+# Source needed
+source /home/vpaadmin/.bashrc
 # You can add other necessary environment variables or commands here
 
-exec "$@"
+roslaunch vpa_demo fast_start_smart_intersection.launch
+
