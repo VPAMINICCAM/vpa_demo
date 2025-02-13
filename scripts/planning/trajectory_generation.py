27ab13e12661b598f62870d64eeb5752fa9b96a4
Yikai Zeng <yikai.zeng@tu-dresden.de>
2025-02-13 10:42:59 +0100

    update the trajectory considering the start point

--- scripts/planning/trajectory_generation.py
+++ scripts/planning/trajectory_generation.py
@@ -19,6 +19,21 @@ def generate_trajectory_turn(direction: int) -> list:
             trajectory_points.append(point)
     
     elif direction == 1: # left turn
+        duration = 0.21 / speed  # 20 cm straight
+        num_points = int(duration * num_points_per_meter)
+        for i in range(num_points + 1):
+            t = i * (duration / num_points)
+            x = speed * t - 0.06
+            y = 0
+            theta = 0  # Pointing along the positive x-axis
+            
+            point = [x, y, theta, t]
+            
+            trajectory_points.append(point)        
+
+        end_x = x
+        end_y = y
+
         turn_radius = 0.3  # 30 cm radius
         arc_length = math.pi / 2 * turn_radius
         turn_duration = arc_length / speed
@@ -27,18 +42,41 @@ def generate_trajectory_turn(direction: int) -> list:
         for i in range(num_turn_points + 1):
             t = i * (turn_duration / num_turn_points)
             theta = (math.pi / 2) * (t / turn_duration)  # Turning left
-            x = turn_radius * math.sin(theta)
-            y = turn_radius * (1 - math.cos(theta))
+            x = turn_radius * math.sin(theta) + end_x
+            y = turn_radius * (1 - math.cos(theta)) + end_y
             
             point = [x, y, theta, t]
             
             trajectory_points.append(point)
+        end_y = y
+        duration = 0.15 / speed  # 15 cm straight
+        for i in range(num_points + 1):
+            t = i * (duration / num_points)
+            y = speed * t + end_y
+            
+            point = [x, y, theta, t]
+            
+            trajectory_points.append(point)   
     
     elif direction == -1: # right turn
         turn_radius = 0.15  # 15 cm radius
         arc_length = math.pi / 2 * turn_radius
         turn_duration = arc_length / speed
         num_turn_points = int(turn_duration * num_points_per_meter)
+        duration = 0.06 / speed  # 15 cm straight
+        num_points = int(duration * num_points_per_meter)
+        for i in range(num_points + 1):
+            t = i * (duration / num_points)
+            x = speed * t - 0.06
+            y = 0
+            theta = 0  # Pointing along the positive x-axis
+            
+            point = [x, y, theta, t]
+            
+            trajectory_points.append(point)        
+
+        end_x = x
+        end_y = y        
         
         for i in range(num_turn_points + 1):
             t = i * (turn_duration / num_turn_points)
